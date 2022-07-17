8736cef93ff4477b107dee477b03daa482bdb12f
ucanbaklava <wexeyeze@gmail.com>
2022-07-17 13:25:14 +0300

    feat: disable popup window when the cuurrent tab url does not match with host_permissions

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
+chrome.action.disable();
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(() => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: chrome.runtime.getManifest().host_permissions.map(h => {
+        const [, sub, host] = h.match(/:\/\/(\*\.)?([^/]+)/);
+        return new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: sub ? {hostSuffix: '.' + host} : {hostEquals: host},
+        });
+      }),
+      actions: [new chrome.declarativeContent.ShowAction()],
+    }]);
+  });
+});
+
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
     chrome.scripting.executeScript({
